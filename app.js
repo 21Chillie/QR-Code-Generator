735bc279b5fc8182238d61a8ef3474de9107169c
Chillie <reskialamsyah@gmail.com>
2025-07-18 14:52:50 +0800

    --Under Development--

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import { createWriteStream } from "node:fs";
 import express from "express";
+import axios from "axios";
 import bodyParser from "body-parser";
-import qr from "qr-image";
 
 const app = express();
 const PORT = 3000;
+const API_URL = "http://api.qrserver.com/v1/create-qr-code/";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,26 +12,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-	res.render("index.ejs", { content: null, filePath: null });
+	res.render("index", { content: null });
 });
 
-app.post("/generate", (req, res) => {
+app.post("/generate", async (req, res) => {
 	try {
-		const text = req.body.text;
-		const fileName = "user-qr";
+		const response = await axios.get(API_URL, {
+			responseType: "arraybuffer",
+			params: {
+				data: req.body.userText,
+				format: req.body.fileType,
+			},
+		});
 
-		// Generate QR Code Image from text variable to directory public/images/filename.svg
-		qr.image(text, { type: "svg" }).pipe(createWriteStream(`public/images/qr-image/${fileName}.svg`));
+		console.log(response.data)
 
-		const filePath = `/images/qr-image/${fileName}.svg`;
+		const base64Image = Buffer.from(response.data, "binary").toString("base64");
+		const contentType = response.headers["content-type"]; // e.g. image/png
+		const dataURI = `data:${contentType};base64,${base64Image}`;
 
-		res.render("index", { content: "Generate Success", filePath: filePath });
+		res.render("index", { content: dataURI });
 	} catch (error) {
-		console.error("QR Code Generation Failed:", error);
-		res.render("index", { content: "Oops! Something went wrong on our end.", filePath: null });
+		console.error("Failed to fetch QR code:", error.response);
+		res.sendStatus(500);
 	}
 });
 
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port http://localhost:${PORT}`);
 });
